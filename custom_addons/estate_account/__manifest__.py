2bffb5d54828885a0b413f4a7f1af5a66b2b0b22
Mashkhurbek <mashkhurbek.mashrabov@gmail.com>
2024-03-28 15:36:22 +0500

    Chapter 14: Interact With Other Modules

---
+++ custom_addons/estate_account/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Real Estate Account',
+    'version': '1.0',
+    'category': 'Sales/CRM',
+    'sequence': 2,
+    'summary': 'Track leads and close opportunities',
+    'description': "",
+    'license': "LGPL-3",
+    'depends': [
+        'account',
+        'estate',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+}
